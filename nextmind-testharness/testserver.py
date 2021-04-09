052f0846ed6cf3f0647aa6236f77a1f010fb9cab
Chris Lloyd-Jones <19361656+Sealjay-clj@users.noreply.github.com>
2021-04-09 16:09:03 +0100

    refactor(requirements): added ngrok to requirements

---
+++ nextmind-testharness/testserver.py
@@ -0,0 +1,19 @@
+from pyngrok import ngrok
+# Open a HTTP tunnel on the default port 80
+# <NgrokTunnel: "http://<public_sub>.ngrok.io" -> "http://localhost:80">
+http_tunnel = ngrok.connect()
+# Open a SSH tunnel
+# <NgrokTunnel: "tcp://0.tcp.ngrok.io:12345" -> "localhost:22">
+ssh_tunnel = ngrok.connect(22, "tcp")
+
+
+
+ngrok_process = ngrok.get_ngrok_process()
+
+try:
+    # Block until CTRL-C or some other terminating event
+    ngrok_process.proc.wait()
+except KeyboardInterrupt:
+    print(" Shutting down server.")
+
+    ngrok.kill()

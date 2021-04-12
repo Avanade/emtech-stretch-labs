c8455dea97235512f88f43a20c5c070fa5a78d8b
Fergus Kidd <fergus.e.kidd@avanade.com>
2021-04-12 11:40:44 +0100

    build(controlagent): attempt to utilise grpc

--- control-agent/MoveCommands.py
+++ control-agent/MoveCommands.py
@@ -1,11 +1,15 @@
 import grpc
-import teletubby_pb2.py
-import teletubby_pb2_grpc.py
+import teletubby_pb2
+import teletubby_pb2_grpc
 
 
 def turnLeft():
     channel = grpc.insecure_channel("api2.rocos.io:443")
-    print(channel)
+    destination = "/ros/cmd_vel"  # check left turn command
+
+    stub = teletubby_pb2_grpc.GreeterStub(channel)
+    response = stub.SendTelemetry(teletubby_pb2.SendTelemetry(context=destination))
+    print("Greeter client received: " + response.message)
 
 
 turnLeft()

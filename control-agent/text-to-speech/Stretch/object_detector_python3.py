39b397aafc30cad674893f9cb5af7ab995b853a1
Fergus Kidd <45766226+FergusKidd@users.noreply.github.com>
2021-04-20 10:56:19 +0100

    refactor(cognitive-services): remove speech key object detection

--- control-agent/text-to-speech/Stretch/object_detector_python3.py
+++ control-agent/text-to-speech/Stretch/object_detector_python3.py
@@ -58,10 +58,7 @@ def getVisionKeys():
 
 def speak(text):
     key, region = getSpeechKeys()
-    # speech_config = SpeechConfig(subscription=key, region=region)
-    speech_config = SpeechConfig(
-        subscription="06eb9311f8c94b088627778387860715", region="uksouth"
-    )
+    speech_config = SpeechConfig(subscription=key, region=region)
     speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
     audio_config = AudioOutputConfig(use_default_speaker=True)
     synthesizer = SpeechSynthesizer(
@@ -80,6 +77,39 @@ def uploadBlob(blobBytes):
     )
 
     blob.upload_blob(blobBytes)
+
+
+def recognize(blobData):
+
+    key, url = getVisionKeys()
+
+    headers = {
+        # Request headers
+        "Content-Type": "application/octet-stream",
+        "Ocp-Apim-Subscription-Key": key,
+    }
+
+    params = urllib.parse.urlencode(
+        {
+            # Request parameters
+            "visualFeatures": "Description",
+            "language": "en",
+        }
+    )
+
+    body = blobData
+
+    try:
+        conn = http.client.HTTPSConnection(url)
+        conn.request("POST", "/vision/v3.1/analyze?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("Vision Error: ", e)
+
+    return json.loads(data)
 
 
 class ObjectDetector:

0d739c4b32418aa76d723f1b873d25f0f21b4e7d
Fergus Kidd <45766226+FergusKidd@users.noreply.github.com>
2021-04-22 16:43:13 +0100

    feat(cognitive-services): qna maker for bot experience

--- control-agent/text-to-speech/speech-detection.py
+++ control-agent/text-to-speech/speech-detection.py
@@ -1,16 +1,21 @@
 import azure.cognitiveservices.speech as speechsdk
 import os
 import json
+import requests
 
 import speech
 
 
-def getSpeechKeys():
+def __location__():
     __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__))
     )
+    return __location__
 
-    with open(os.path.join(__location__, "config.json")) as json_file:
+
+def getSpeechKeys():
+
+    with open(os.path.join(__location__(), "config.json")) as json_file:
         data = json.load(json_file)
         key = data["speechKey"]
         region = data["speechLocation"]
@@ -19,17 +24,24 @@ def getSpeechKeys():
 
 
 def getLuisKeys():
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__))
-    )
 
-    with open(os.path.join(__location__, "config.json")) as json_file:
+    with open(os.path.join(__location__(), "config.json")) as json_file:
         data = json.load(json_file)
         key = data["LuisKey"]
         region = data["LuisRegion"]
         appid = data["LuisAppId"]
 
     return key, region, appid
+
+
+def getQnAKeys():
+
+    with open(os.path.join(__location__(), "config.json")) as json_file:
+        data = json.load(json_file)
+        url = data["qnaURL"]
+        key = data["qnaKey"]
+
+    return url, key
 
 
 def from_mic():
@@ -61,6 +73,16 @@ def recognize_intent():
     intent_result = intent_recognizer.recognize_once()
 
     # Check the results
+    try:
+        intentJson = json.loads(intent_result.intent_json)
+    except:
+        return ""
+    print(intentJson)
+    # if low score, do QnA
+    if intentJson["topScoringIntent"]["score"] < 0.9:
+        qna = json.loads(QnA(intent_result.text))
+        return qna["answers"][0]["answer"]
+
     if intent_result.reason == speechsdk.ResultReason.RecognizedIntent:
         print(
             'Recognized: "{}" with intent id `{}`'.format(
@@ -91,17 +113,50 @@ def recognize_intent():
     return intent_result
 
 
+def QnA(question):
+
+    url, key = getQnAKeys()
+    url = url
+
+    payload = '{"question":"' + question + '"}'
+    headers = {
+        "Authorization": key,
+        "Content-Type": "application/json",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.text
+
+
 run = True
 
 while run == True:
 
     intent = recognize_intent()
-    speech.speak(intent.text)
+    try:
+        if intent.intent_id == "Vision":
+            speech.speak("I'm looking")
+            result = speech.recognize(
+                "https://robots.ieee.org/robots/stretch/stretch-1200x630.jpg"
+            )
 
-    if intent.intent_id == "Vision":
-        speech.speak("I'm looking")
-        result = speech.recognize(
-            "https://robots.ieee.org/robots/stretch/stretch-1200x630.jpg"
-        )
+            speech.speak(
+                "I can see" + str(result["description"]["captions"][0]["text"])
+            )
 
-        speech.speak("I can see" + str(result["description"]["captions"][0]["text"]))
+        elif intent.intent_id == "Move":
+
+            intentJson = json.loads(intent.intent_json)
+            try:
+                direction = intentJson["entities"][0]["entity"]
+                speech.speak("I'm going to move " + str(direction))
+            except:
+                speech.speak("I don't know what direction to move")
+
+        elif intent.intent_id == "Weather.QueryWeather":
+            speech.speak("I don't have the capacity for thermoregulation")
+
+    except:
+        if intent != "No good match found in KB.":
+            speech.speak(intent)

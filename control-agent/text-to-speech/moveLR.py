2ce2d875618401fd08ca24a5cab71ad288cb6e95
FergusKidd <45766226+FergusKidd@users.noreply.github.com>
2021-06-21 12:38:35 +0100

    feat(cognitive-services): working camera interaction with Cognitive services and movement

--- control-agent/text-to-speech/moveLR.py
+++ control-agent/text-to-speech/moveLR.py
@@ -2,22 +2,20 @@ import stretch_body.robot
 import time
 import sys
 
-robot=stretch_body.robot.Robot()
+robot = stretch_body.robot.Robot()
 robot.startup()
 rotate_by = 0
 
-print('recieved:',sys.argv[1],sys.argv[2])
+print("recieved:", sys.argv[1], sys.argv[2])
 
-if sys.argv[1] == 'left':
-        rotate_by = float(sys.argv[2])
-elif sys.argv[1] == 'right':
-        rotate_by = -1*float(sys.argv[2])
+if sys.argv[1] == "left":
+    rotate_by = float(sys.argv[2])
+elif sys.argv[1] == "right":
+    rotate_by = -1 * float(sys.argv[2])
 
-if sys.argv[1] == 'right' or 'left':
-        robot.base.rotate_by(rotate_by)
-        robot.push_command()
+if sys.argv[1] == "right" or "left":
+    robot.base.rotate_by(rotate_by)
+    robot.push_command()
 
 time.sleep(2)
 robot.stop()
-
-

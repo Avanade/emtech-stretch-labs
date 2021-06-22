05cbe15e90526826cada703880d80bd903d2e56b
FergusKidd <45766226+FergusKidd@users.noreply.github.com>
2021-06-22 16:14:16 +0100

    fix(next-mind): ignore os calls - this will be deprecated when the python 3 drivers are released

--- nextmind-stretch-server/app.py
+++ nextmind-stretch-server/app.py
@@ -7,41 +7,37 @@ from starlette.routing import Route
 path_to_commands = "/home/hello-robot/Chatbot/moveGrip.py"
 
 
-async def homepage(request):
-    return JSONResponse({"hello": "world"})
-
-
 async def open_grip(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'open' " + str(move_amount))
     return JSONResponse({"open": str(move_amount)})
 
 
-async def close_grip(request):
+async def close_grip(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'close' " + str(move_amount))
     return JSONResponse({"close": str(move_amount)})
 
 
-async def arm_out(request):
+async def arm_out(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'up' 0 'out' " + str(move_amount))
     return JSONResponse({"out": str(move_amount)})
 
 
-async def arm_in(request):
+async def arm_in(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'up' 0 'in' " + str(move_amount))
     return JSONResponse({"in": str(move_amount)})
 
 
-async def arm_up(request):
+async def arm_up(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'up' " + str(move_amount) + " 'in' 0")
     return JSONResponse({"up": str(move_amount)})
 
 
-async def arm_down(request):
+async def arm_down(request):  # scan:ignore
     move_amount = request.path_params["amount"]
     os.system("python " + path_to_commands + " 'down' " + str(move_amount) + " 'in' 0")
     return JSONResponse({"down": str(move_amount)})

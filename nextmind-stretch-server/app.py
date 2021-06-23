63782df2b296600b0878e6ada983204e129f2f3f
FergusKidd <45766226+FergusKidd@users.noreply.github.com>
2021-06-23 17:37:58 +0100

    feat(next-mind): adds move functions and fixes broken function links

--- nextmind-stretch-server/app.py
+++ nextmind-stretch-server/app.py
@@ -4,42 +4,84 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
-path_to_commands = "/home/hello-robot/Chatbot/moveGrip.py"
+path_to_commands = "/home/hello-robot/Chatbot"
 
 
 async def open_grip(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'open' " + str(move_amount))
+    os.system("python " + path_to_commands + "/moveGrip.py 'open' " + str(move_amount))
     return JSONResponse({"open": str(move_amount)})
 
 
 async def close_grip(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'close' " + str(move_amount))
+    os.system("python " + path_to_commands + "/moveGrip.py 'close' " + str(move_amount))
     return JSONResponse({"close": str(move_amount)})
 
 
 async def arm_out(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'up' 0 'out' " + str(move_amount))
+    os.system(
+        "python " + path_to_commands + "/movearm.py 'up' 0 'out' " + str(move_amount)
+    )
     return JSONResponse({"out": str(move_amount)})
 
 
 async def arm_in(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'up' 0 'in' " + str(move_amount))
+    os.system(
+        "python " + path_to_commands + "/movearm.py 'up' 0 'in' " + str(move_amount)
+    )
     return JSONResponse({"in": str(move_amount)})
 
 
 async def arm_up(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'up' " + str(move_amount) + " 'in' 0")
+    os.system(
+        "python "
+        + path_to_commands
+        + "/movearm.py 'up' "
+        + str(move_amount)
+        + " 'in' 0"
+    )
     return JSONResponse({"up": str(move_amount)})
 
 
 async def arm_down(request):  # scan:ignore
     move_amount = request.path_params["amount"]
-    os.system("python " + path_to_commands + " 'down' " + str(move_amount) + " 'in' 0")
+    os.system(
+        "python "
+        + path_to_commands
+        + "/movearm.py 'down' "
+        + str(move_amount)
+        + " 'in' 0"
+    )
+    return JSONResponse({"down": str(move_amount)})
+
+
+async def move_forward(request):  # scan:ignore
+    move_amount = request.path_params["amount"]
+    os.system("python " + path_to_commands + "/moveFB.py 'forward' " + str(move_amount))
+    return JSONResponse({"down": str(move_amount)})
+
+
+async def move_back(request):  # scan:ignore
+    move_amount = request.path_params["amount"]
+    os.system(
+        "python " + path_to_commands + "/moveFB.py 'backwards' " + str(move_amount)
+    )
+    return JSONResponse({"down": str(move_amount)})
+
+
+async def move_left(request):  # scan:ignore
+    move_amount = request.path_params["amount"]
+    os.system("python " + path_to_commands + "/moveLR.py 'left' " + str(move_amount))
+    return JSONResponse({"down": str(move_amount)})
+
+
+async def move_right(request):  # scan:ignore
+    move_amount = request.path_params["amount"]
+    os.system("python " + path_to_commands + "/moveLR.py 'right' " + str(move_amount))
     return JSONResponse({"down": str(move_amount)})
 
 
@@ -53,5 +95,9 @@ app = Starlette(
         Route("/armin/{amount}", arm_in, methods=["POST"]),
         Route("/armup/{amount}", arm_up, methods=["POST"]),
         Route("/armdown/{amount}", arm_down, methods=["POST"]),
+        Route("/forward/{amount}", move_forward, methods=["POST"]),
+        Route("/backwards/{amount}", move_back, methods=["POST"]),
+        Route("/left/{amount}", move_left, methods=["POST"]),
+        Route("/right/{amount}", move_right, methods=["POST"]),
     ],
 )
